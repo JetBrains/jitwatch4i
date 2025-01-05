59101d0df595a19befb17ceef4a5c884522d1a0f
Vladimir Dvorak <vladimir.dvorak@jetbrains.com>
2025-01-05 18:18:31 +0100

    Scroll to end of log on start

--- src/main/java/org/adoptopenjdk/jitwatch/ui/log/LogPanel.java
+++ src/main/java/org/adoptopenjdk/jitwatch/ui/log/LogPanel.java
@@ -4,6 +4,8 @@ import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.components.JBTextArea;
 
 import javax.swing.*;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 import java.awt.*;
 
 public class LogPanel extends JPanel
@@ -17,6 +19,19 @@ public class LogPanel extends JPanel
         textAreaLog = new JBTextArea();
         textAreaLog.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
         add(new JBScrollPane(textAreaLog), BorderLayout.CENTER);
+
+        this.addAncestorListener(new AncestorListener() {
+            @Override
+            public void ancestorAdded(AncestorEvent event)
+            {
+                SwingUtilities.invokeLater(() -> textAreaLog.setCaretPosition(textAreaLog.getDocument().getLength()));
+            }
+            @Override
+            public void ancestorRemoved(AncestorEvent event) {}
+            @Override
+            public void ancestorMoved(AncestorEvent event) {}
+        });
+
     }
 
     public void clear()
